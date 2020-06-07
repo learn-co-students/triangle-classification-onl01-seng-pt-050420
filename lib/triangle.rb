1f1117c9a1c5e653c48115cb26a0f31b6b5f5c67
kschemel2010 <kschemel2010@gmail.com>
2020-06-06 20:48:24 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,58 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  @sides = []
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if invalid == true
+      begin
+       raise TriangleError
+       rescue TriangleError => error
+        puts error.message
+      end
+    elsif(equilateral)
+      :equilateral
+    elsif(scalene)
+      :scalene
+    elsif(isosceles)
+      :isosceles
+    end
+  end
+
+  def invalid
+    @sides.each do |length|
+      if length <= 0 && @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 
+         true
+      else
+        false
+        end
+      end
+    end
+ 
+    #
+
+
+    def equilateral
+      @side1 == @side2 && @side1 == @side3
+    end
+
+    def scalene
+      @side1 != @side2 && @side1!= @side3 && @side2 != @side3
+    end
+
+    def isosceles
+      @side1 == @side2 || @side1 = @side3 || @side2 == @side3
+    end
+
+  
+end
+
+class TriangleError < StandardError
+  "you must give the get_married method an argument of an instance of the person class!"
 end
