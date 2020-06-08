21930561de66e51d18d22518fb643bb5ee7bc7cd
Catheryne Small <catheryn.small@gmail.com>
2020-06-08 04:13:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c, :equilateral, :isosceles, :scalene
+  
+  def initialize(side_a, side_b, side_c)
+      @side_a = side_a
+      @side_b = side_b
+      @side_c = side_c
+  end
+  
+  def kind 
+    sides = [side_a, side_b, side_c]
+    true_triangle = [side_a+side_b > side_c, side_a + side_c > side_b, side_b + side_c > side_a]
+    
+    if sides.min <= 0 || true_triangle.any?(false)
+      raise TriangleError 
+    end
+    
+    if @side_a == @side_b && @side_a == @side_c 
+      :equilateral
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  
+  class TriangleError < StandardError
+    def message
+      "Length must be greater than zero."
+    end
+  end
 end
