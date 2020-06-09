1d8990941c52a822325ae9d683178a22a4ffb345
kschemel2010 <kschemel2010@gmail.com>
2020-06-08 18:23:11 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,61 +1,27 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
-  @sides = []
-  # include TriangleError
-
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-    @sides = [side1, side2, side3]
-    # include TriangleError
-  end
-  def kind
-    if self.invalid == true
-      # begin
-       raise TriangleError
-      #  rescue TriangleError => error
-      #   puts error.message
-      # end
-    elsif equilateral
-      :equilateral
-    elsif scalene 
-      :scalene
-    elsif isosceles
-      :isosceles
-    end
-  end
-
-  def invalid
-    @sides.each do |length|
-      if length <= 0 || @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1 
-         true
-      else
-        false
-        end
-      end
-    end
- 
-    #
-
-
-    def equilateral
-      @side1 == @side2 && @side1 == @side3
-    end
-
-    def scalene
-      @side1 != @side2 && @side1!= @side3 && @side2 != @side3
-    end
-
-    def isosceles
-      @side1 == @side2 || @side1 = @side3 || @side2 == @side3
-    end
-
+  attr_reader :side_1, :side_2, :side_3
   
-end
-
-class TriangleError < StandardError
-  def message
-   "Side length must be greater than 0.  The sum of two sides must be greater than the third."
+  def initialize(side_1, side_2, side_3)
+    @side_1  = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
-end
+
+  def kind
+    if @side_1 + @side_1 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2 || @side_1 < 0 || @side_2 < 0 || @side_3 < 0
+      raise TriangleError
+    elsif @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3
+      :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+      :isosceles
+    else @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+      :scalene
+    end 
+  end 
+ 
+
+  class TriangleError < StandardError
+  end 
+end 
+
+
